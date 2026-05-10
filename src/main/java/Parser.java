fa26e3a14201d175aa78814b9623a9284860771a
Johan Paul <johanjohn405@gmail.com>
2026-05-10 09:30:12 +0530

    Optimized Parser and Edited Comments for Readability

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -3,14 +3,15 @@ import java.util.List;
 import java.util.regex.*;
 
 public class Parser {
-
     public List<Student> parser(List<String> lines){
         Subject subject;
         Student student;
         List<Student> students = new ArrayList<>();
 
+        //main loop that detects roll no., if not detected, skip line
         for (int lineIndex = 0; lineIndex < lines.size(); lineIndex++){
             String[] tokens = lines.get(lineIndex).trim().split("\\s+");
+            if (tokens.length == 0) continue;
             if (tokens[0].matches("\\d{8}")){
                 String roll = tokens[0];
                 String gender = tokens[1];
@@ -19,7 +20,7 @@ public class Parser {
                 List<Subject> subjects = new ArrayList<>();
                 StringBuilder name = new StringBuilder();
 
-                //this loop keeps reading tokens until subject code is detected
+                //this loop keeps reading tokens (names) until subject code is detected
                 for (int i=2; i<tokens.length; i++){
                     if (tokens[i].matches("\\d{3}")) {
                         subjectIndex = i;
@@ -28,7 +29,7 @@ public class Parser {
                     name.append(tokens[i]).append(" ");
                 }
 
-                //this loop keeps reading tokens until internal grades are detected
+                //this loop keeps reading tokens (subject codes) until internal grades are detected
                 List<String> subjectCodes = new ArrayList<>();
                 for (; subjectIndex<tokens.length; subjectIndex++){
                     if (tokens[subjectIndex].matches("\\d{3}")) subjectCodes.add(tokens[subjectIndex]);
@@ -38,7 +39,7 @@ public class Parser {
                     }
                 }
 
-                //this loop stores the internal grades until result is detected
+                //this loop reads and stores the internal grades until result is detected
                 List<String> internalGradeCodes = new ArrayList<>();
                 for (; internalGradeIndex<tokens.length; internalGradeIndex++){
                     if (tokens[internalGradeIndex].matches("[A-F][1-2]?")) internalGradeCodes.add(tokens[internalGradeIndex]);
@@ -47,17 +48,23 @@ public class Parser {
 
                 //store result
                 String result = tokens[internalGradeIndex];
+                lineIndex++;
 
 
                 //Parsing Marks, 2nd Line
-                if (lineIndex + 1 >= lines.size() ) continue;
-                String[] marksLine = lines.get(lineIndex + 1).trim().split("\\s+");
+                if (lineIndex >= lines.size()) continue;
+                String[] marksLine = lines.get(lineIndex).trim().split("\\s+");
                 List<String> marks = new ArrayList<>();
                 List<String> grades = new ArrayList<>();
                 for (int i=0; i<marksLine.length; i++){
                     if (marksLine[i].matches("\\d{3}")) marks.add(marksLine[i]);
                     else if (marksLine[i].matches("[A-F][1-2]?")) grades.add(marksLine[i]);
                     else break;
+                }
+
+                if (marks.size() != subjectCodes.size() || grades.size() != subjectCodes.size()) {
+                    System.out.println("Data has mismatch, skipping row");
+                    continue;
                 }
 
                 //create subject objects
